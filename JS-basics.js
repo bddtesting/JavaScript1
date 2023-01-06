34c8d71dae3b6befc49f14c026c9dd090a851426
Rohit <bddtesting2022@gmail.com>
2023-01-06 13:29:34 -0500

    first commit

---
+++ JS-basics.js
@@ -0,0 +1,34 @@
+var name = 'Adam'
+name1 ='Eva'
+
+var age = 15 // number
+var email = 'sna@test.com'
+var address = '123 main st, san jose, ca 21345'
+var sum = 15 + 20 // 35
+var x = 'a' + 15
+
+var doo = 'hello' + 'world'
+var isHuman = true // Boolean
+isHuman = false 
+
+var isVaccinated = false
+
+var ageofPerson = 40
+
+var x1 = 1
+var y1 = 2
+
+var z1 = x1 + y1 //
+
+var z2 = z1 - x1 // 
+
+console.log('Hello World')
+console.log(x)
+console.log(10+40)
+
+var xx = 100
+var yy = 200
+
+console.log(xx*yy)
+
+console.log(xx == 50)
